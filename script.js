b08c9b1f2b12ebefc30f2bf32f39369d552213e7
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-09 10:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function onesDigit(n) {
   // your code here
+	let nstring = n.toString();
+	return nstring.slice(-1)
 }
 
 // Do not change the code below
 
 const n = prompt("Enter Number:");
+
 alert(onesDigit(n));
